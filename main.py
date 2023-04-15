0d95599e89c8b1031f1886daa2a0516a58972337
Arman Bolatov <a.bol@bc-pf.org>
2023-04-16 02:11:44 +0600

    added endpoint

--- main.py
+++ main.py
@@ -193,3 +193,17 @@ async def send_sms(sms_data: SmsInput, token: str = Depends(get_token_from_env))
         raise HTTPException(status_code=response.status_code, detail="Error sending SMS")
     
 
+@app.get("/get_phone_number/{iin}")
+async def get_phone_number(iin: str, token: str = Depends(get_token_from_env)):
+    url = f"http://hakaton.gov4c.kz/api/bmg/check/{iin}/"
+    headers = {"Authorization": f"Bearer {token}"}
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+
+    if response.status_code == 200:
+        phone_number = response.json()
+        return phone_number
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Error getting phone number")
+    
